0395b8fec2b1f1694be6d989f76b9eda337391dc
lesogorovalex <galanthusan@gmail.com>
2020-07-20 20:11:41 +0300

    ClockFace update

--- index.js
+++ index.js
@@ -10,30 +10,31 @@ class CountdownTimer {
       seconds: document.querySelector('span[data-value="secs"]'),
     };
     this.timer();
+    this.updateClockface();
   }
+  updateClockface = function () {
+    this.startTime = Date.now();
+    this.time = this.targetDate.getTime() - this.startTime;
 
-  timer() {
-    this.intervalId = setInterval(() => {
-      this.startTime = Date.now();
-      this.time = this.targetDate.getTime() - this.startTime;
+    this.days = Math.floor(this.time / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor(
+      (this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
 
-      this.days = Math.floor(this.time / (1000 * 60 * 60 * 24));
-      this.hours = Math.floor(
-        (this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-      );
+    this.minutes = Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60));
+    this.seconds = Math.floor((this.time % (1000 * 60)) / 1000);
 
-      this.minutes = Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60));
-      this.seconds = Math.floor((this.time % (1000 * 60)) / 1000);
-
-      this.refs.days.textContent = String(this.days).padStart(2, '0');
-      this.refs.hours.textContent = String(this.hours).padStart(2, '0');
-      this.refs.minutes.textContent = String(this.minutes).padStart(2, '0');
-      this.refs.seconds.textContent = String(this.seconds).padStart(2, '0');
-    }, 1000);
-  }
+    this.refs.days.textContent = String(this.days).padStart(2, '0');
+    this.refs.hours.textContent = String(this.hours).padStart(2, '0');
+    this.refs.minutes.textContent = String(this.minutes).padStart(2, '0');
+    this.refs.seconds.textContent = String(this.seconds).padStart(2, '0');
+  };
+  timer = function () {
+    this.intervalId = setInterval(this.updateClockface.bind(this), 1000);
+  };
 }
 
 new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 20, 2020'),
+  targetDate: new Date('Jul 25, 2020'),
 });
